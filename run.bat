cd00df3a01392bcc53a0350690c6e741c6f2be8e
Aizen <symryvvin@gmail.com>
2018-02-10 17:49:27 +0300

    Add splash screen

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 set JAVA_HOME=D:\jdk-9.0.4\bin
-set JAVA_CMD=%JAVA_HOME%\java
+set JAVA_CMD=%JAVA_HOME%\javaw
 
-start %JAVA_CMD% -jar target\SpringFX-1.0.jar
+start %JAVA_CMD% -jar target\SpringFXExample-1.0.jar
--- src/main/java/org/name/app/Launcher.java
+++ src/main/java/org/name/app/Launcher.java
@@ -1,24 +1,35 @@
 package org.name.app;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 
 public class Launcher extends Application {
     private static ClassPathXmlApplicationContext context;
+    private Stage splashScreen;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     /**
-     * Инициализируем контекст
+     * Контекст инициализируется не в UI потоке. Поэтому в методе init() UI поток вызывается через Platform.runLater()
+     * @throws Exception
      */
     @Override
-    public void init() {
+    public void init() throws Exception {
+        Platform.runLater(this::showSplash);
+        Thread.sleep(10000);
         context = new ClassPathXmlApplicationContext("application-context.xml");
+        Platform.runLater(this::closeSplash);
     }
 
     @Override
@@ -30,7 +41,30 @@ public class Launcher extends Application {
      * Освобождаем контекст
      */
     @Override
-    public void stop() throws IOException {
+    public void stop() {
         context.close();
+    }
+
+    /**
+     * Загружаем заставку обычным способом. Выставляем везде прозрачность
+     */
+    private void showSplash() {
+        try {
+            splashScreen = new Stage(StageStyle.TRANSPARENT);
+            splashScreen.setTitle("Splash");
+            Parent root = FXMLLoader.load(getClass().getResource("/view/fxml/splash.fxml"));
+            Scene scene = new Scene(root, Color.TRANSPARENT);
+            splashScreen.setScene(scene);
+            splashScreen.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Закрывает сцену с заставкой
+     */
+    private void closeSplash() {
+        splashScreen.close();
     }
 }
